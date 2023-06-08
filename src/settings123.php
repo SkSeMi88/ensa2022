02e375a7b6aae857a5f2aa7078e621c1306b39ae
SkSeMi88 <45976267+SkSeMi88@users.noreply.github.com>
2023-06-08 13:30:28 +0300

    Update settings123.php

--- src/settings123.php
+++ src/settings123.php
@@ -3,26 +3,26 @@
 return [
 	// 'db' => [
 	// 	'host'		=> 'localhost',
-	// 	// 'dbname'	=> 'kb2',
-	// 	'dbname'	=> 'kb2021',
-	// 	'user'		=> 'root',
-	// 	'password'	=> 'root',
+	// 	// 'dbname'	=> '************',
+	// 	'dbname'	=> '**********',
+	// 	'user'		=> '***********',
+	// 	'password'	=> '************',
 	// ]
 
 	'db' => [
 		'host'		=> 'localhost',
-		// 'dbname'	=> 'kb2',
-		'dbname'	=> 'cq28624_ensa',
-		'user'		=> 'root',
-		'password'	=> 'root',
+		// 'dbname'	=> '********',
+		'dbname'	=> '**********',
+		'user'		=> '***********',
+		'password'	=> '***************',
      ]
   /*
   	'db' => [
 		'host'		=> 'localhost',
-		// 'dbname'	=> 'kb2',
-		'dbname'	=> 'cq28624_testkb',
-		'user'		=> 'cq28624_testkb',
-		'password'	=> 'cq28624_testkb',
+		// 'dbname'	=> '*****************',
+		'dbname'	=> '*****************',
+		'user'		=> '******************',
+		'password'	=> '*****************',
      ]
      */
 ];
